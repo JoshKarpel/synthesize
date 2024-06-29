c9d78bea69ecffd8926919ed84dc8521835485cf
Josh Karpel <josh.karpel@gmail.com>
2024-06-28 20:32:53 -0500

    print execution duration

--- synthesize/messages.py
+++ synthesize/messages.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from pydantic import Field
 from watchfiles import Change
@@ -20,6 +20,7 @@ class ExecutionCompleted(Message):
     node: FlowNode
     pid: int
     exit_code: int
+    duration: timedelta
 
 
 class ExecutionOutput(Message):
